307e9524dc3db08888b3725e0c2cc8214eba817a
GaryT25 <truegarythompson@gmail.com>
2022-11-09 09:37:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var pikachu = 'Pikachu'
+var pikachuAttack = 20;
+var pikachuHealth = 100;
+var charizard = 'Charizard'
+var charizardAttack = 40;
+var charizardHealth = 100;
+
+document.getElementById("pikachuHealth").innerHTML = pikachuHealth;
